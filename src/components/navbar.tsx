5fee9634fba97a98f20948371658d8fc80283eb4
GuillaumeFlp <filipe.guillaume@gmail.com>
2024-10-05 15:32:36 +0200

    feat: about and experiences

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,55 +7,127 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Dock, DockIcon } from "@/components/ui/dock";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import {
+  CodeIcon,
+  GitHubLogoIcon,
+  HomeIcon,
+  LinkedInLogoIcon,
+  ReaderIcon,
+} from "@radix-ui/react-icons";
+import { Link } from "react-router-dom";
+import { RoutePaths } from "@/routes/routes-constants";
 
 export default function Navbar() {
   return (
     <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
       <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
       <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
-        {/* {DATA.navbar.map((item) => (
-          <DockIcon key={item.href}>
+        <DockIcon>
+          <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
-                  href={item.href}
+                  to={RoutePaths.HOME}
                   className={cn(
                     buttonVariants({ variant: "ghost", size: "icon" }),
-                    "size-12"
+                    "size-10"
                   )}
                 >
-                  <item.icon className="size-4" />
+                  <HomeIcon className="size-5" />
                 </Link>
               </TooltipTrigger>
               <TooltipContent>
-                <p>{item.label}</p>
+                <p>Accueil</p>
               </TooltipContent>
             </Tooltip>
-          </DockIcon>
-        ))} */}
+          </TooltipProvider>
+        </DockIcon>
+        <DockIcon>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  to={RoutePaths.HOME}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-10"
+                  )}
+                >
+                  <CodeIcon className="size-5" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Projets</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DockIcon>
+        <DockIcon>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  to={RoutePaths.HOME}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-10"
+                  )}
+                >
+                  <ReaderIcon className="size-5" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Blog</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DockIcon>
         <Separator orientation="vertical" className="h-full" />
-        {/* {Object.entries(DATA.contact.social)
-          .filter(([_, social]) => social.navbar)
-          .map(([name, social]) => (
-            <DockIcon key={name}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href={social.url}
-                    className={cn(
-                      buttonVariants({ variant: "ghost", size: "icon" }),
-                      "size-12"
-                    )}
-                  >
-                    <social.icon className="size-4" />
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>{name}</p>
-                </TooltipContent>
-              </Tooltip>
-            </DockIcon>
-          ))} */}
+        <DockIcon>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <a
+                  href="https://github.com/GuillaumeFlp"
+                  target="_blank"
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-10"
+                  )}
+                >
+                  <GitHubLogoIcon className="size-5" />
+                </a>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>GitHub</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DockIcon>
+        <DockIcon>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <a
+                  href="https://www.linkedin.com/in/guillaume-filipe-175698131"
+                  target="_blank"
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-10"
+                  )}
+                >
+                  <LinkedInLogoIcon className="size-5" />
+                </a>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>LinkedIn</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </DockIcon>
+
         <Separator orientation="vertical" className="h-full py-2" />
         <DockIcon>
           <TooltipProvider>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,11 @@
 import Navbar from "@/components/navbar";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import BlurFade from "@/components/ui/blur-fade";
+import BlurFadeText from "@/components/ui/blur-fade-text";
+
+const BLUR_FADE_DELAY = 0.04;
 
 function HomePage() {
   return (
@@ -6,7 +13,153 @@ function HomePage() {
       id="home-page"
       className="min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6"
     >
-      <h1>CC</h1>
+      <main className="flex flex-col min-h-[100dvh] space-y-10">
+        <section id="hero">
+          <div className="mx-auto w-full max-w-2xl space-y-8">
+            <div className="gap-2 flex justify-between">
+              <div className="flex-col flex flex-1 space-y-1.5">
+                <BlurFadeText
+                  delay={BLUR_FADE_DELAY}
+                  className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                  yOffset={8}
+                  text="Guillaume Filipe 🚀"
+                />
+                <BlurFadeText
+                  className="max-w-[600px] md:text-xl"
+                  delay={BLUR_FADE_DELAY}
+                  text="Ingénieur logiciel fullstack spécialisé dans le développement web."
+                />
+              </div>
+              <BlurFade delay={BLUR_FADE_DELAY}>
+                <Avatar className="size-28 border">
+                  <AvatarImage
+                    alt="Guillaume Filipe"
+                    src="/resume/images/profil.jpeg"
+                  />
+                  <AvatarFallback>GF</AvatarFallback>
+                </Avatar>
+              </BlurFade>
+            </div>
+          </div>
+        </section>
+
+        <section id="about">
+          <BlurFade delay={BLUR_FADE_DELAY * 3}>
+            <h2 className="text-xl font-bold">A propos</h2>
+          </BlurFade>
+          <BlurFadeText
+            className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
+            delay={BLUR_FADE_DELAY * 4}
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+        </section>
+
+        <section id="skills">
+          <div className="flex min-h-0 flex-col gap-y-3">
+            <BlurFade delay={BLUR_FADE_DELAY * 4}>
+              <h2 className="text-xl font-bold">Skills</h2>
+            </BlurFade>
+            <div className="flex flex-wrap gap-1">
+              {[
+                "React",
+                "Typescript",
+                "Node.js",
+                "Postgres",
+                "Docker",
+                "TODO",
+              ].map((skill, id) => (
+                <BlurFade key={skill} delay={BLUR_FADE_DELAY * 5 + id * 0.05}>
+                  <Badge key={skill}>{skill}</Badge>
+                </BlurFade>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section id="work">
+          <div className="flex min-h-0 flex-col gap-y-3">
+            <BlurFade delay={BLUR_FADE_DELAY * 5}>
+              <h2 className="text-xl font-bold">
+                Expériences professionnelles
+              </h2>
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 6}>
+              <ResumeCard
+                logoUrl="/resume/images/bam_karaoke_box.png"
+                altText="BAM Karaoke Box"
+                title="BAM Karaoke Box"
+                subtitle="Senior Software Engineer"
+                period="Mai 2023 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 7}>
+              <ResumeCard
+                logoUrl="/resume/images/popchef.png"
+                altText="Popchef"
+                title="Popchef"
+                subtitle="Software Engineer"
+                period="Novembre 2022 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 8}>
+              <ResumeCard
+                logoUrl="/resume/images/thales.png"
+                altText="Thales"
+                title="Thales"
+                subtitle="Software Engineer"
+                period="Novembre 2022 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 9}>
+              <ResumeCard
+                logoUrl="/resume/images/thales.png"
+                altText="Thales"
+                title="Thales"
+                subtitle="Alternance"
+                period="Novembre 2022 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 10}>
+              <ResumeCard
+                logoUrl="/resume/images/weiss.jpg"
+                altText="Weiss Technik France"
+                title="Weiss Technik France"
+                subtitle="Stage"
+                period="Novembre 2022 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+          </div>
+        </section>
+
+        <section id="education">
+          <div className="flex min-h-0 flex-col gap-y-3">
+            <BlurFade delay={BLUR_FADE_DELAY * 11}>
+              <h2 className="text-xl font-bold">Formations</h2>
+            </BlurFade>
+
+            <BlurFade delay={BLUR_FADE_DELAY * 12}>
+              <ResumeCard
+                logoUrl="/resume/images/esiee.jpg"
+                altText="ESIEE Paris"
+                title="ESIEE Paris"
+                subtitle="Formation ingénieur informatique"
+                period="Mai 2023 - Mai 2023"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </BlurFade>
+          </div>
+        </section>
+      </main>
       <Navbar />
     </div>
   );
